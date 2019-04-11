d7df39c13384c038c263119da74eac8733c7c088
unknown <jan@ueberruhr.de>
2019-04-11 10:18:09 +0200

    load fxml Data

--- src/main/java/fahrradverleih/Main.java
+++ src/main/java/fahrradverleih/Main.java
@@ -1,8 +1,24 @@
 package fahrradverleih;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application {
+
     public static void main(String[] args){
-        System.out.println("hello world");
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fahrradVerleih.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 }
